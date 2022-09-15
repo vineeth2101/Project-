994b644b9754aaec1de2dc93f1302f4747037294
vineeth2101 <89122550+vineeth2101@users.noreply.github.com>
2022-09-15 16:00:30 +0530

    Update buildImg.sh

--- buildImg.sh
+++ buildImg.sh
@@ -1,4 +1,4 @@
-REG_USERNAME="Vineeth2101"
+REG_USERNAME="vineeth2101"
 REG_PASSWORD="Hitachi@123#"
 echo "====> Logging into conatiner"
 sudo docker login --username $REG_USERNAME --password $REG_PASSWORD
@@ -12,9 +12,9 @@ if [ $? -ne 0 ] ; then
         echo "ERROR: Docker build failed"
         exit 1
 fi
-sudo docker tag angularv1:av1 Vineeth2101/angul:av1
+sudo docker tag angularv1:av1 vineeth2101/angul:av1
 echo "====> Inintialising Pushing the image to conatiner"
-sudo docker push Vineeth2101/angul:av1
+sudo docker push vineeth2101/angul:av1
 if [ $? -ne 0 ] ; then
         echo "ERROR: Docker PUSH failed"
         exit 1
